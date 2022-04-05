b99e931b4e6496a62976023f95ffdac58d53b26d
Ahmad Fathoni <fat@fathoni.com>
2022-04-05 22:44:09 +0700

    fix RentCar

--- src/com/SourceCode/RentCar/CarData.java
+++ src/com/SourceCode/RentCar/CarData.java
@@ -13,14 +13,14 @@ public class CarData {
     }
 
     public void listOfCar(){
-        System.out.println("-------------------------");
+        System.out.println("------------------------------");
         System.out.println("DAFTAR MOBIL");
-        System.out.println("-------------------------");
+        System.out.println("------------------------------");
         for (int i = 0; i < carList.size(); i++) {
             System.out.println("TIPE MOBIL : "+carList.get(i).getCarType());
             System.out.println("NO. POLISI : "+carList.get(i).getPolNum());
             System.out.println("MERK MOBIL : "+carList.get(i).getMerk());
-            System.out.println("-------------------------");
+            System.out.println("------------------------------");
         }
     }
 
--- src/com/SourceCode/RentCar/RentArchive.java
+++ src/com/SourceCode/RentCar/RentArchive.java
@@ -18,15 +18,15 @@ public class RentArchive {
     }
 
     public void info(){
-        System.out.println("-------------------------");
+        System.out.println("------------------------------");
         System.out.println("INFORMASI PELANGGAN");
-        System.out.println("-------------------------");
+        System.out.println("------------------------------");
         for (int i = 0; i < rentData.size(); i++) {
             System.out.println("NAMA PEMINJAM : "+rentData.get(i).getRider().getName());
             System.out.println("TIPE MOBIL    : "+rentData.get(i).getCar().getCarType());
             System.out.println("NO. POLISI    : "+rentData.get(i).getCar().getPolNum());
             System.out.println("LAMA RENTAL   : "+rentData.get(i).getRentDur());
-            System.out.println("-------------------------");
+            System.out.println("------------------------------");
         }
     }
 }
