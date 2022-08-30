3f9e78c2b185716378ed14df263f4572f1f177da
Ahmad Fathoni <fat@fathoni.com>
2022-08-30 07:32:09 +0700

    commit

--- src/com/SourceCode/CobaCoba.java
+++ src/com/SourceCode/CobaCoba.java
@@ -1,15 +1,31 @@
 package com.SourceCode;
 
 import java.util.*;
-
 public class CobaCoba{
 
    public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
+      boolean condition = true;
 
-      System.out.print("Input");
-      String coba = scan.nextLine();
-      System.out.println(coba);
+      while (condition) {
+         System.out.print("Masukkan angka : ");
+         int angka = scan.nextInt();
+         if (angka < 0) {
+            System.out.println("bilangan negatif");
+         }else if(angka > 0){
+            System.out.println("bilangan positif");
+         }else{
+            System.out.println("angka 0");
+         }
+
+         int ulang = 0;
+         System.out.println("ulangi ? 1:yes / 2:no");
+         System.out.print("input : ");
+         ulang = scan.nextInt();
+         if (ulang == 2) {
+            condition = false;
+         }
+      }
 
    }
 }
